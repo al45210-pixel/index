2c92c3c6039cf1a6ef2a0732fc7fef79f3648d12
al45210-pixel <al45210@un.edu.mx>
2026-07-02 21:32:20 -0600

    ewq

---
+++ dom.js
@@ -0,0 +1,15 @@
+
+
+mensaje = function(){
+
+    var nombre = document.getElementById("nombre")
+    alert(nom);
+    alert(nom.value);
+
+    
+
+}
+
+
+var boton = document.getElementById("btnCalcular")
+boton.addEventListener("click", mensaje);
